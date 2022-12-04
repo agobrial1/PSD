1d9b9903783f9eec366b5337590bdfdbdd559859
agobrial1 <agobrial@outlook.com>
2022-12-04 17:54:10 +0100

    This is my first commit

---
+++ 6_a_random_int_gen.py
@@ -0,0 +1,30 @@
+# First commit
+
+import random
+# method to generate a list with random numbers
+
+class Random:
+
+    def __init__(self, amount, minRange, maxRange):
+        
+        # array container
+        self.randomList = []
+        # amount of random numbers to generate
+        for i in range(amount):
+            # generate random number
+            self.randomList.append(random.randint(minRange, maxRange))
+        # print array
+        print(self.randomList) 
+
+# create object
+
+amount = int(input("Enter the amount of random numbers: "))
+minRange = int(input("Enter the minimum range: "))
+maxRange = int(input("Enter the maximum range: "))
+
+Random(amount, minRange, maxRange)
+
+'''
+Pre-Conditions - amount input must be integer above 1, and minRange cannot equal maxRange.
+Post-Conditions - The program will print an array of random numbers, and returned values are integers.
+'''
